5cf1447a0e1fea93e4ca9fab1b04e27b37466b6b
Esdras Júnior <eblsj@cesar.org.br>
2020-06-19 12:50:09 -0300

    client: Adapt Storage interface to use HTTP layer

--- src/Storage.js
+++ src/Storage.js
@@ -1,105 +1,19 @@
-import axios from 'axios';
-import url from 'url';
+import HTTP from './network/HTTP';
 
-function throwError(message) {
-  const error = new Error(message);
-  throw error;
-}
-
-function extractSettings({ hostname, port, protocol, id, token, pathname }) {
-  const defaultProtocol = 'https';
-  const defaultPort = 443;
-
-  if (!hostname) {
-    throwError('A hostname should be provided');
-  }
-  if (!id || !token) {
-    throwError('Both id and token should be provided');
-  }
-
-  return {
-    hostname,
-    port: !port ? defaultPort : port,
-    protocol: !protocol ? defaultProtocol : protocol,
-    id,
-    token,
-    pathname: pathname || '',
-  };
-}
-
-function buildUri(protocol, hostname, port, pathname, query) {
-  return url.format({
-    protocol,
-    hostname,
-    port,
-    pathname,
-    query,
-  });
-}
-
-class Storage {
-  constructor(options) {
-    this.options = extractSettings(options);
+export default class Client {
+  constructor(config) {
+    this.http = new HTTP(config);
   }
 
   async listData(query) {
-    const uri = buildUri(
-      this.options.protocol,
-      this.options.hostname,
-      this.options.port,
-      `${this.options.pathname}/data`,
-      query
-    );
-    const headers = {
-      auth_id: this.options.id,
-      auth_token: this.options.token,
-    };
-
-    const response = await axios.get(uri, { headers });
-    return response.data;
+    return this.http.get('/data', query);
   }
 
-  async listDataByDevice(id, query) {
-    if (!id) {
-      throwError('A device ID should be provided');
-    }
-
-    const uri = buildUri(
-      this.options.protocol,
-      this.options.hostname,
-      this.options.port,
-      `${this.options.pathname}/data/${id}`,
-      query
-    );
-    const headers = {
-      auth_id: this.options.id,
-      auth_token: this.options.token,
-    };
-
-    const response = await axios.get(uri, { headers });
-    return response.data;
+  async listDataByDevice(thingId, query) {
+    return this.http.get(`/data/${thingId}`, query);
   }
 
-  async listDataBySensor(deviceId, sensorId, query) {
-    if (!deviceId || sensorId === undefined) {
-      throwError('Both device ID and sensor ID should be provided');
-    }
-
-    const uri = buildUri(
-      this.options.protocol,
-      this.options.hostname,
-      this.options.port,
-      `${this.options.pathname}/data/${deviceId}/sensor/${sensorId}`,
-      query
-    );
-    const headers = {
-      auth_id: this.options.id,
-      auth_token: this.options.token,
-    };
-
-    const response = await axios.get(uri, { headers });
-    return response.data;
+  async listDataBySensor(thingId, sensorId, query) {
+    return this.http.get(`/data/${thingId}/sensor/${sensorId}`, query);
   }
 }
-
-export default Storage;
